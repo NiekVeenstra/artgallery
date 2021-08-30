4323b9f294bd1169c30581cee5befd4e5f7b5c86
Niek <n.veenstra02@gmail.com>
2021-08-30 20:11:25 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div className="footer">
-            <h1>footer</h1>
+  return (
+    <div className="footer">
+      <div className="footer__container">
+        <div className="footerContent">
+          <div className="footerRow">
+            <h3>pages</h3>
+            <ul>
+              <li>
+                {" "}
+                <Link className="footerLink" to="/">
+                  Work
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className="footerLink" to="/about">
+                  About
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link className="footerLink" to="/contact">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footerRow">
+              <h3>links</h3>
+          </div>
+          <div className="footerRow">
+              <h3>social</h3>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
