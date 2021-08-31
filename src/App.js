8483309b5c724015b6b67a7920934130a0de3942
Niek <n.veenstra02@gmail.com>
2021-08-31 17:36:14 +0200

    add temporary page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from "./components/Footer";
 import Homepage from "./pages/Homepage";
 import Aboutpage from "./pages/Aboutpage";
 import Contactpage from "./pages/Contactpage";
+import Test from "./pages/Test";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
           <Route exact path="/" component={Homepage} />
           <Route exact path="/about" component={Aboutpage} />
           <Route exact path="/contact" component={Contactpage} />
+          <Route exact path="/test" component={Test} />
         </Switch>
         <Footer />
       </div>
---
+++ src/pages/Test.jsx
@@ -0,0 +1,205 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div className="testpage">
+      <ul class="timeline">
+        <li>
+          <div class="direction-r">
+            <div class="flag-wrapper">
+              <span class="hexa"></span>
+              <span class="flag">S</span>
+              <span class="time-wrapper">
+                <span class="time activetime">0%</span>
+              </span>
+            </div>
+            <div class="desc rounded">
+              SLOTHz airdrop: 50 SLOTHz for early community members and ambassadors.
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="direction-l">
+            <div class="flag-wrapper">
+              <span class="hexa"></span>
+              <span class="flag">L</span>
+              <span class="time-wrapper">
+                <span class="time">10%</span>
+              </span>
+            </div>
+            <div class="desc rounded">
+              Improve our Discord Server with boosting, dedicated Management, SLOTHz memes contest
+              and some bots. We will make our first adoption, adopting{" "}
+              <a
+                href="https://slothconservation.org/product/becket/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Beckett
+              </a>{" "}
+              and sending it to one of our early adopters.
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="direction-r">
+            <div class="flag-wrapper">
+              <span class="hexa"></span>
+              <span class="flag">O</span>
+              <span class="time-wrapper">
+                <span class="time">20%</span>
+              </span>
+            </div>
+            <div class="desc rounded">
+              We will host a community meetup for ideas, including a 20 SLOTHz giveaway for
+              participants and 2 ETH worth of custom swag!
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="direction-l">
+            <div class="flag-wrapper">
+              <span class="hexa"></span>
+              <span class="flag">T</span>
+              <span class="time-wrapper">
+                <span class="time">30%</span>
+              </span>
+            </div>
+            <div class="desc rounded">
+              We will start our first giveaway for 10 lucky SLOTHz Hodlers of 1 ETH each on a
+              livestream. We’re also adopting{" "}
+              <a
+                href="https://slothconservation.org/product/ali-jessica/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Ali &amp; Jessica
+              </a>
+              , to one of our hodlers.
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="direction-r">
+            <div class="flag-wrapper">
+              <span class="hexa"></span>
+              <span class="flag">H</span>
+              <span class="time-wrapper">
+                <span class="time">40%</span>
+              </span>
+            </div>
+            <div class="desc rounded">
+              Forging the future! We will deploy a forging webapp so everyone can create custom
+              SLOTHz as part of our plan to let YOU create your own Sloth and reduce the total
+              supply.
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="direction-l">
+            <div class="flag-wrapper">
+              <span class="hexa"></span>
+              <span class="flag">z</span>
+              <span class="time-wrapper">
+                <span class="time">50%</span>
+              </span>
+            </div>
+            <div class="desc rounded">
+              It can’t only be games right? Right? We’re setting up a $20,000 fund to design and
+              develop our own digital club "The Zoo". We will adopt{" "}
+              <a
+                href="https://slothconservation.org/product/bojangles/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Bojangles
+              </a>
+              , a cute Sloth for one of our members!
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="direction-r">
+            <div class="flag-wrapper">
+              <span class="hexa"></span>
+              <span class="flag">4</span>
+              <span class="time-wrapper">
+                <span class="time">60%</span>
+              </span>
+            </div>
+            <div class="desc rounded">
+              We will be giving 1 lucky hodler a PSA 10 Charizard Base Set Unlimited WOTC.
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="direction-l">
+            <div class="flag-wrapper">
+              <span class="hexa"></span>
+              <span class="flag">L</span>
+              <span class="time-wrapper">
+                <span class="time">70%</span>
+              </span>
+            </div>
+            <div class="desc rounded">
+              The Zoo will start taking shape! We will setup an extra $10,000 fund for giveaways and
+              treasure hunts for members.
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="direction-r">
+            <div class="flag-wrapper">
+              <span class="hexa"></span>
+              <span class="flag">I</span>
+              <span class="time-wrapper">
+                <span class="time">80%</span>
+              </span>
+            </div>
+            <div class="desc rounded">
+              Welcome the baby-slothz collection! We will start the works to extend our
+              art-collection and bring the babies to the table! Hodlers will be able to mint a baby,
+              but every baby needs to have 2 parents so watch out!
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="direction-l">
+            <div class="flag-wrapper">
+              <span class="hexa"></span>
+              <span class="flag">F</span>
+              <span class="time-wrapper">
+                <span class="time">90%</span>
+              </span>
+            </div>
+            <div class="desc rounded">
+              We will host a treasure hunt for SLOTHz Hodlers on our website to win 5 ETH!
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="direction-r">
+            <div class="flag-wrapper">
+              <span class="hexa"></span>
+              <span class="flag">E</span>
+              <span class="time-wrapper">
+                <span class="time">100%</span>
+              </span>
+            </div>
+            <div class="desc rounded">
+              Jackpot prize for 10 random Hodlers including (but not limited to): 5 days vacation to
+              anywhere in the world (2 people), 20 ETH prize pool &amp; 20 SLOTHz. Plus we will
+              adopt every Sloth at{" "}
+              <a href="https://slothconservation.org/" target="_blank" rel="noopener noreferrer">
+                https://slothconservation.org/
+              </a>{" "}
+              for a full year!
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Test;
